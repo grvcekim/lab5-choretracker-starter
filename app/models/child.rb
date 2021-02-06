1fced1c33cd139e5053f27f47ffa597e553265d9
Grace Kim <gracekim@Graces-MacBook-Pro.local>
2021-02-05 20:02:21 -0500

    added points_earned method to Child

--- app/models/child.rb
+++ app/models/child.rb
@@ -6,7 +6,12 @@ class Child < ApplicationRecord
 
   scope :alphabetical, -> { order(:last_name, :first_name) }
   scope :active, -> { where(active: true) }
+
   def name
     first_name + " " + last_name
+  end
+
+  def points_earned
+    self.chores.done.inject(0){|sum,chore| sum += chore.task.points}
   end
 end
