4a46443b087b42c32f9dcb01a61af77dce1c94c1
Grace Kim <gracekim@Graces-MacBook-Pro.local>
2021-02-05 19:43:00 -0500

    pass all Task tests

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,5 +5,6 @@ class Task < ApplicationRecord
   validates_presence_of :name
   validates_numericality_of :points, only_integer: true, greater_than_or_equal_to: 0
 
-
+  scope :alphabetical, -> { order(:name) }
+  scope :active, -> { where(active: true) }
 end
