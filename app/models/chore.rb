0298009a0ef11834060b7419cdfd2cffcce20527
Grace Kim <gracekim@Graces-MacBook-Pro.local>
2021-02-05 19:58:50 -0500

    pass Chore scope tests

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -3,4 +3,11 @@ class Chore < ApplicationRecord
   belongs_to :task
 
   validates_date :due_on
+  
+  scope :by_task, -> { joins(:task).order("tasks.name") }
+  scope :chronological, -> { order(:due_on) }
+  scope :pending, -> { where(completed: false) }
+  scope :done, -> { where(completed: true )}
+  scope :upcoming, -> { where("due_on >= ?", Date.current) }
+  scope :past, -> { where("due_on < ?", Date.current) }
 end
